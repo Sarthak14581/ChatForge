a3e0c9db2ffc6ad6431b898c76b0a4cc2c9a1bc8
Sarthak14581 <sarthakzunjurke@gmail.com>
2026-01-08 16:00:06 +0530

    Add Database Connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
@@ -11,12 +12,48 @@ app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
+  connectDB();
 });
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("connected with Database!.")
+  } catch (error) {
+    console.log("Failed to connect with DB",error);
+  }
 
-app.post("/test", async (req, res) => {
+  
+}
 
-});
+
+// app.post("/test", async (req, res) => {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer  ${process.env.OPENAI_API_KEY}`,
+//     },
+//     body: JSON.stringify({
+//       model: "gpt-4o-mini",
+//       messages: [
+//         {
+//           role: "user",
+//           content: req.body.message,
+//         },
+//       ],
+//     }),
+//   };
+
+//   try {
+//     const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+//     const data = await response.json();
+//     console.log(data.choices[0].message.content);
+//     res.send(data.choices[0].message.content);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 // import OpenAI from 'openai';
 // import { config } from 'dotenv';
