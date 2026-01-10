0349db9584c85f3af225c91f0b04827ba1e3e7ec
Sarthak14581 <sarthakzunjurke@gmail.com>
2026-01-10 10:41:24 +0530

    Add sidebar with css

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,7 +1,32 @@
 import "./Sidebar.css";
+import logoImage from "./assets/blacklogo.png";
 
 export default function Sidebar() {
-    return <div>
-        sidebar
-    </div>
+  return (
+    <section className="sidebar">
+      {/* new chat button */}
+      <button>
+        <img src={logoImage} alt="chatForge logo" className="logo" />
+        <span>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </span>
+      </button>
+
+      {/* history */}
+
+      <ul className="history">
+        <li>thread_1</li>
+        <li>thread_2</li>
+        <li>thread_3</li>
+        <li>thread_4</li>
+        <li>thread_5</li>
+      </ul>
+
+      {/* sign */}
+
+      <div className="sign">
+        <p>By Sarthak &hearts; </p>
+      </div>
+    </section>
+  );
 }
